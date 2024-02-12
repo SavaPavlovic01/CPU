3399a224cc26f35f2c4247f391ef2546c04088cb
SavaPavlovic01 <124643557+SavaPavlovic01@users.noreply.github.com>
2024-02-12 19:02:12 +0100

    Update top.v

--- src/synthesis/modules/top.v
+++ src/synthesis/modules/top.v
@@ -1,49 +1,94 @@
-module top #(parameter DIVISOR=50000000, parameter FILE_NAME="mem_init.mif",
-             parameter ADDR_WIDTH=6, parameter DATA_WIDTH=16) (
+module top #(
+    parameter DIVISOR = 50_000_000,
+    parameter FILE_NAME = "mem_init.mif",
+    parameter ADDR_WIDTH = 6,
+    parameter DATA_WIDTH = 16,
+    parameter ADDR_HI = ADDR_WIDTH - 1,
+    parameter DATA_HI = DATA_WIDTH - 1
+) (
     input clk,
-    input rst_n,
-    input[2:0] btn,
+    input [2:0] btn,
     input [9:0] sw,
     output [9:0] led,
     output [27:0] hex
 );
 
-    wire clk_slowed;
-    clk_div #(DIVISOR) clk_div_inst(.clk(clk), .rst_n(rst_n), .out(clk_slowed));
+wire clk_div_wire;
+assign rst_n = sw[9];
 
-    wire mem_we;
-    wire [ADDR_WIDTH-1:0] mem_addr;
-    wire [DATA_WIDTH-1:0] mem_data;
-    wire [DATA_WIDTH-1:0] mem_out;
-    wire [ADDR_WIDTH-1:0] pc, sp;
+clk_div #(DIVISOR) CLK_DIV(
+    .clk(clk),
+    .rst_n(rst_n),
+    .out(clk_div_wire)
+);
 
-    wire [DATA_WIDTH-1:0] cpu_out;
+wire [DATA_HI:0] mem_data, mem_in;
+wire [ADDR_HI:0] mem_addr;
+wire mem_we;
+wire [10:0] cpu_out_visak;
+wire [5:0] pc_out, sp_out;
 
-    assign led[4:0] = cpu_out[4:0];
-    memory memory_inst(clk_slowed, mem_we, mem_addr, mem_data, mem_out);
+cpu #(ADDR_WIDTH, DATA_WIDTH) CPU(
+    .clk(clk_div_wire), //
+    .rst_n(rst_n),
+    .mem_in(mem_in),
+    .in({{12{1'b0}},sw[3:0]}),
+    .mem_we(mem_we),
+    .mem_addr(mem_addr),
+    .mem_data(mem_data),
+    .out({cpu_out_visak, led[4:0]}),
+    .pc(pc_out),
+    .sp(sp_out)
+);
 
-    wire [3:0] sp_tens, sp_ones;
-    wire [3:0] pc_tens, pc_ones;
+memory #(FILE_NAME, ADDR_WIDTH, DATA_WIDTH) MEMORY(
+    .clk(clk_div_wire), //
+    .we(mem_we),
+    .addr(mem_addr),
+    .data(mem_data),
+    .out(mem_in)
+);
 
-    bcd bcd_sp(.in(sp), .tens(sp_tens), .ones(sp_ones));
-    bcd bcd_pc(.in(pc), .tens(pc_tens), .ones(pc_ones));
 
-    ssd ssd_sp_tens (.in(sp_tens), .out(hex[27:21]));
-    ssd ssd_sp_ones (.in(sp_ones), .out(hex[20:14]));
-    ssd ssd_pc_tens (.in(pc_tens), .out(hex[13:7]));
-    ssd ssd_pc_ones (.in(pc_ones), .out(hex[6:0]));
+wire [5:0] BCD0_in, BCD1_in;
+wire [3:0] BCD0_ones, BCD0_tens, BCD1_ones, BCD1_tens;
+bcd BCD0 (
+    .in(sp_out),
+    .tens(BCD0_tens),
+    .ones(BCD0_ones)
+);
+bcd BCD1 (
+    .in(pc_out),
+    .tens(BCD1_tens),
+    .ones(BCD1_ones)
+);
 
-    cpu #(ADDR_WIDTH, DATA_WIDTH) cpu_inst (
-        .clk(clk_slowed),
-        .rst_n(sw[9]),
-        .mem_in(mem_out),
-        .in({{(DATA_WIDTH-4){1'b0}},{sw[3:0]}}),
-        .mem_we(mem_we),
-        .mem_addr(mem_addr),
-        .mem_data(mem_data),
-        .out(cpu_out),
-        .pc(pc),
-        .sp(sp)
-    );
-    
+//assign BCD0.in = CPU.sp;
+//assign BCD1.in = CPU.pc;
+
+ssd SSD0 (
+    .in(BCD0_tens),
+    .out(hex[27:21]));
+ssd SSD1 (
+    .in(BCD0_ones),
+    .out(hex[20:14]));
+ssd SSD2 (
+    .in(BCD1_tens),
+    .out(hex[13:7]));
+ssd SSD3 (
+    .in(BCD1_ones),
+    .out(hex[6:0]));
+
+
+/*
+assign SSD0.in = BCD0_tens;
+assign SSD1.in = BCD0_ones;
+assign SSD2.in = BCD1_tens;
+assign SSD3.in = BCD1_ones;
+*/
+//assign hex[27:21] = SSD0.out;
+//assign hex[20:14] = SSD1.out;
+//assign hex[13:7]  = SSD2.out;
+//assign hex[6:0]   = SSD3.out;
+
 endmodule
